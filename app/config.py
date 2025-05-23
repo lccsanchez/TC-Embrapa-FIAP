b4cd4926615b63de37421981ef1586585c57eb28
valdrei <valdreisalomao@hotmail.com>
2025-05-23 00:51:07 -0300

    alterado arquivos para execucao no render

--- app/config.py
+++ app/config.py
@@ -1,21 +1,22 @@
 import os
-from dotenv import load_dotenv #Usado para carregar variáveis de ambiente do arquivo .env
+from dotenv import load_dotenv
 from urllib.parse import quote_plus
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Configuração da URL do banco de dados
 DB_USER = os.getenv("DB_USER", "azureuser")
-DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "your_password_here"))
-DB_HOST = os.getenv("DB_HOST", "tcembrapadb.database.windows.net")
-DB_PORT = os.getenv("DB_PORT", "1433")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+DB_HOST = os.getenv("DB_HOST", "tcembrapadb.mysql.database.azure.com")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "tcembrapadb")
+SSL_CA = os.getenv("SSL_CA", "certs/DigiCertGlobalRootCA.crt.pem")  # Caminho padrão
 
-# String de conexão para Azure SQL com pyodbc
+# String de conexão para Azure MySQL com pymysql
 SQLALCHEMY_DATABASE_URI = (
-    f"mssql+pyodbc://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    "?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes&TrustServerCertificate=no&Connection+Timeout=30"
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?ssl_ca={SSL_CA}"
 )
+print(SQLALCHEMY_DATABASE_URI)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
