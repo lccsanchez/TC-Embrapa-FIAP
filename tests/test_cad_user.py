b5257684062c0d5809a63cc9a4b59021cc5241d6
valdrei <valdreisalomao@hotmail.com>
2025-05-23 00:51:30 -0300

    remocao de arquivos não utilizados

--- tests/test_cad_user.py
+++
@@ -1,7 +0,0 @@
-from app.database import SessionLocal
-from app.database_operations import inserir_usuario
-import os
-
-session = SessionLocal()
-inserir_usuario(session, os.getenv("usuario"), os.getenv("senha"))
-session.close()
