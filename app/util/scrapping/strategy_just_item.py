086db78020fa7bed2efc1cdf4f617971bf5dc06e
dinnamusdev <fernandozoe180@yahoo.com>
2025-05-30 11:43:14 -0300

    ajuste do scapper para os cenarios importacao e exportacao

--- app/util/scrapping/strategy_just_item.py
+++ app/util/scrapping/strategy_just_item.py
@@ -8,9 +8,11 @@ class JustItems:
     """Estratégia para o scraping do tipo 2."""
 
     def scrape(self, html_content: str):
+        empty_data = True
         """Realiza o scraping de itens simples."""
         soup = BeautifulSoup(html_content, "html.parser")
         items = soup.find(class_="tb_base tb_dados")
+        empty_values = ["-","0"]
         results = {}
         if not items:
             return results
@@ -18,9 +20,13 @@ class JustItems:
         for item in items.find_all("tr"):
             data = item.find_all("td")
             if len(data) >= 3:
+                quantidade = data[1].text.strip()
+                valor =data[2].text.strip()
+                empty_data = False if not quantidade in empty_values or not valor in empty_values else True
                 pais = data[0].text.strip()
                 results[pais] = {
-                    "Quantidade": data[1].text.strip(),
-                    "Valor": data[2].text.strip(),
+                    "Quantidade": quantidade,
+                    "Valor": valor,
                 }
-        return results
+               
+        return [] if empty_data else results
